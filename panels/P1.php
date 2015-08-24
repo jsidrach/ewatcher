b6c9c638ae98dfe358e23ca6138a945035b59e3a
JSidrach <juan.sidrach@gmail.com>
2015-08-24 11:44:54 +0200

    Panel 1 (sin front-end)

--- panels/P1.php
+++ panels/P1.php
@@ -18,25 +18,50 @@
       // Graphic: sPLoad (last 7 values + interactivity)
       // Value: eDLoad
       // Graphic: eDLoad (last 7 values + interactivity)
-      echo "TODO P1";
-      echo strval($this->feeds['sPLoad']['id']);
       ?>
-      <span class="instant-feed" data-feedid="357">Test</span>
-      <div id="test"></div>
-      <div id="test2"></div>
+      <div>
+        <span>sPLoad:</span>
+        <span class="instant-feed" data-feedid="<?php echo $this->feeds['sPLoad']['id']; ?>">
+          <?php echo $this->feeds['sPLoad']['value']; ?>
+        </span>
+        <span> (units)</span>
+      </div>
+      <div>
+        <span>sVoltage:</span>
+        <span class="instant-feed" data-feedid="<?php echo $this->feeds['sVoltage']['id']; ?>">
+          <?php echo $this->feeds['sVoltage']['value']; ?>
+        </span>
+        <span> (units)</span>
+      </div>
+      <div id="sPLoad"></div>
+      <div>
+        <span>eDLoad:</span>
+        <span class="instant-feed" data-feedid="<?php echo $this->feeds['eDLoad']['id']; ?>">
+          <?php echo $this->feeds['eDLoad']['value']; ?>
+        </span>
+        <span> (units)</span>
+      </div>
+      <div id="eDLoad"></div>
       <script>
         $(window).on('load', function() {
-          var feeds = [
+          // sPLoad graphic
+          var sPLoad = [
             {
-              id: 598,
-              color: "#122223",
-              legend: "Test legend 2"
-            },
-            {
-              id: 602
+              id: <?php echo $this->feeds['sPLoad']['id']; ?>,
+              color: "#00ABE3",
+              legend: "sPLoad"
             }
           ];
-          FeedChartFactory.create("test", feeds, {chartType: "daily"});
+          FeedChartFactory.create("sPLoad", sPLoad);
+          // eDLoad graphic
+          var eDLoad = [
+            {
+              id: <?php echo $this->feeds['eDLoad']['id']; ?>,
+              color: "#00E37D",
+              legend: "eDLoad"
+            }
+          ];
+          FeedChartFactory.create("eDLoad", eDLoad, {chartType: "daily"});
         });
       </script>
       <?php
