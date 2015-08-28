026bb0df0b74ed7db8815a827f883d5c56bb8990
JSidrach <juan.sidrach@gmail.com>
2015-08-28 11:09:12 +0200

    Translations, docs scheme

--- ewatcher_translations.php
+++ ewatcher_translations.php
@@ -3,7 +3,7 @@
   function ewatcher_translate($string) {
     $ewatcher_domain = "messages";
     bindtextdomain($ewatcher_domain, "Modules/ewatcher/locale");
-    bind_textdomain_codeset($ewatcher_domain, 'UTF-8');
+    bind_textdomain_codeset($ewatcher_domain, "UTF-8");
     return dgettext($ewatcher_domain, $string);
   }
 ?>
